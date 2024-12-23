f76a6b74009a9ece336634ec8c9351b05951e3bf
imakhlaq <imakhlaqXD@gmail.com>
2024-12-23 22:56:15 +0530

    added more robust exception handling and added server messages

--- backend/src/main/java/com/eventmanagement/exception/handlers/GlobalExceptionHandler.java
+++ backend/src/main/java/com/eventmanagement/exception/handlers/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.eventmanagement.exception.handlers;
 
 import com.eventmanagement.exception.custom.NoRefreshTokenException;
+import com.eventmanagement.exception.custom.NoUserFoundException;
 import com.eventmanagement.response.error.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,12 +16,31 @@ import java.security.GeneralSecurityException;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @Value("${internal-server-error-message}")
+    private String internalServerErrorMessage;
 
     @ExceptionHandler(NoRefreshTokenException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleNoRefreshTokenException(NoRefreshTokenException e, HttpServletRequest request) {
 
+        log.error("User has no refresh token");
+        var customException = ErrorResponse.builder()
+            .message(e.message)
+            .statusCode(e.statusCode)
+            .timestamp(LocalDateTime.now())
+            .path(request.getContextPath())
+            .build();
+        return ResponseEntity.badRequest().body(customException);
+    }
+
+    @ExceptionHandler(NoUserFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleNoUserFoundException(NoUserFoundException e, HttpServletRequest request) {
+
+        log.error("User doesn't exists");
         var customException = ErrorResponse.builder()
             .message(e.message)
             .statusCode(e.statusCode)
@@ -32,8 +54,9 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleGeneralSecurityException(GeneralSecurityException e, HttpServletRequest request) {
 
+        log.error("GeneralSecurityException cause {}", e.getMessage());
         var customException = ErrorResponse.builder()
-            .message(e.getMessage())
+            .message(internalServerErrorMessage)
             .statusCode(HttpStatus.INTERNAL_SERVER_ERROR)
             .timestamp(LocalDateTime.now())
             .path(request.getContextPath())
@@ -45,8 +68,9 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception e, HttpServletRequest request) {
 
+        log.error("Exception cause {}", e.getMessage());
         var customException = ErrorResponse.builder()
-            .message(e.getMessage())
+            .message(internalServerErrorMessage)
             .statusCode(HttpStatus.INTERNAL_SERVER_ERROR)
             .timestamp(LocalDateTime.now())
             .path(request.getContextPath())
